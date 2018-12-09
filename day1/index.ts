997672e70d42a828128c40e6678d25e494794bce
Mattias Buelens <mattias@buelens.com>
2018-12-09 22:38:36 +0100

    Refactor day 1

--- day1/index.ts
+++ day1/index.ts
@@ -5,18 +5,31 @@ const readFile = promisify(fs.readFile);
 
 (async () => {
     const input = await readFile('./input', {encoding: 'utf8'});
-    const changes = input.trim().split('\n').map(x => parseInt(x, 10));
+    const lines = input.trim().split('\n');
+    console.log(`part1: ${part1(lines)}`);
+    console.log(`part2: ${part2(lines)}`);
+})();
+
+function part1(lines: string[]): number {
+    let result = 0;
+    for (let line of lines) {
+        result += parseInt(line, 10);
+    }
+    return result;
+}
+
+function part2(lines: string[]): number {
+    const changes = lines.map(x => parseInt(x, 10));
     let frequency = 0;
     let seen = new Set<number>();
     while (true) {
         for (let change of changes) {
             frequency += change;
             if (seen.has(frequency)) {
-                console.log(frequency);
-                return;
+                return frequency;
             } else {
                 seen.add(frequency);
             }
         }
     }
-})();
+}
